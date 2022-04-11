1faf5ef7c11b03d5ca9960d3b1ce1e7dc268a243
VITTOii <103334183+VITTOii@users.noreply.github.com>
2022-04-11 20:52:07 +0200

    Main

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from traceback import print_tb
-
+import pygame
 
 print('Hello world')
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                False
+
+    pygame.quit()
+
