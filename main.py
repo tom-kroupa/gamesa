6ee05eae461a5773938203ca8f8a7464220f7dd0
VITTOii <103334183+VITTOii@users.noreply.github.com>
2022-04-27 17:34:36 +0200

    Main

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ SKY_BLUE = (50, 153, 204)
 FPS = 60
 VEL = 5
 
-
+# letadlo obrázek, velikost a otočení
 aircraft_image = pygame.image.load(os.path.join("images", "aircraft.png"))
 aircraft_image = pygame.transform.rotate(pygame.transform.scale(aircraft_image, (99, 72)), 270)
 
-def draw_window(bomber):
-    WIN.fill(SKY_BLUE)
+background = pygame.image.load(os.path.join("images", "background.png"))
+
+
+
+def draw_window(bomber): #vykreslí věci ve hře
+    WIN.blit(background, (0, 0))
     WIN.blit(aircraft_image, (bomber.x, bomber.y))
     pygame.display.update()
 
@@ -35,10 +39,10 @@ def main():
                 sys.exit()
 
         key_pressed = pygame.key.get_pressed()
-        if key_pressed[pygame.K_a]: #Left
+        if key_pressed[pygame.K_a] and bomber.x - VEL > 0:  #Left
             bomber.x -= VEL
         draw_window(bomber)
-        if key_pressed[pygame.K_d]: #Right
+        if key_pressed[pygame.K_d] and bomber.x + VEL < 900: #Right odečíst od celku velikost lodi / 900 - bomber rozměry
             bomber.x += VEL
         draw_window(bomber)
         if key_pressed[pygame.K_w]: #UP
