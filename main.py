53ab9f940d89841b24e096ef0101147fa2958b84
VITTOii <103334183+VITTOii@users.noreply.github.com>
2022-05-05 14:19:08 +0200

    Main

--- main.py
+++ main.py
@@ -2,36 +2,43 @@ from traceback import print_tb
 import sys
 import os
 import pygame
+pygame.font.init()
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Anthropos')
 
+font = pygame.font.SysFont("Verdana", 40)
 
 WHITE = (255, 255, 255)
 SKY_BLUE = (50, 153, 204)
+GREEN = (0, 164, 0)
 FPS = 60
 VEL = 6
 bomber_width, bomber_height = (99, 72)
 kubis_vel = 4
+max_kubis = 2
 vydadek = pygame.USEREVENT + 1
-score = 0
+
 
 
 # letadlo obrázek, velikost a otočení
 aircraft_image = pygame.image.load(os.path.join("images", "plane.png"))
 aircraft_image = pygame.transform.rotate(pygame.transform.scale(aircraft_image, (150, 150)), 270)
-
 background = pygame.image.load(os.path.join("images", "background.jpg"))
 les_img = pygame.image.load(os.path.join("images", "forest.png"))
+les_img = pygame.transform.scale(les_img, (250, 250))
 kubis_img = pygame.image.load(os.path.join("images", "kubis.png"))
 kubis_img = pygame.transform.scale(kubis_img, (100, 100))
+bullet_img = pygame.image.load(os.path.join("images", "bullets.png"))
+bullet_img = pygame.transform.rotate(pygame.transform.scale(aircraft_image, (50, 50)), 180)
 
 
-
-def draw_window(bomber, les, kubis_list): #vykreslí věci ve hře
+def draw_window(bomber, les, kubis_list, score): #vykreslí věci ve hře
     WIN.blit(background, (0, 0))
     WIN.blit(les_img, (les.x, les.y))
+    score_text = font.render("Score" + str(score), 1, GREEN)
+    WIN.blit(score_text, (WIDTH - score_text.get_width() - 10, 10))
     WIN.blit(aircraft_image, (bomber.x, bomber.y))
     for kubis in kubis_list:
         WIN.blit(kubis_img, (kubis.x, kubis.y))
@@ -52,23 +59,28 @@ def les_move(les):
     if les.x < -250:
         les.x = 1100
 
+def kubis_handle(kubis_list, les):
+    for kubis in kubis_list:
+        kubis.y = kubis.y + kubis_vel
+        kubis.x = kubis.x - kubis_vel / 3
+        if les.colliderect(kubis):
+            pygame.event.post(pygame.event.Event(vydadek))
+            kubis_list.remove(kubis)
+        elif kubis.y > 500:
+            kubis_list.remove(kubis)
+
+
+
 
 def main():
     pygame.init()
     bomber = pygame.Rect(100, 150, bomber_width, bomber_height)
-    les = pygame.Rect(1000, 250, 30, 30)
+    les = pygame.Rect(1000, 320, 250, 250)
     kubis_list = []
+    score = 0
+
     clock = pygame.time.Clock()
 
-    def kubis_handle(kubis_list, les):
-        for kubis in kubis_list:
-            kubis.y = kubis.y + kubis_vel
-            kubis.x = kubis.x - kubis_vel / 3
-            if les.colliderect(kubis):
-                pygame.event.post(pygame.event.Event(vydadek))
-                kubis_list.remove(kubis)
-            elif kubis.y > 550:
-                kubis_list.remove(kubis)
 
     while True:
         clock.tick(FPS)
@@ -77,19 +89,19 @@ def main():
                 pygame.quit()
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE and len(kubis_list) < max_kubis:
                     kubis = pygame.Rect(bomber.x, bomber.y, 2, 2)
                     kubis_list.append(kubis)
 
-            # if event.type == vydadek:
-            #     score = score + 1
+            if event.type == vydadek:
+                score = score + 1
 
 
         key_pressed = pygame.key.get_pressed() #proměná pro další funkci
         bomber_movement(key_pressed, bomber)   #volání pohybu bomberu
         kubis_handle(kubis_list, les)
         les_move(les)
-        draw_window(bomber, les, kubis_list)
+        draw_window(bomber, les, kubis_list, score)
 
 
 
